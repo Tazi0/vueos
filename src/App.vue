680709a6573794552d96a58ee9f5ed00e84a2a21
Tazio <tazio2003@gmail.com>
2021-06-02 21:09:00 +0200

    App bottom bar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="backgroundIMG" src="http://s1.picswalls.com/wallpapers/2017/12/10/4k-screensaver_110629116_312.jpg" alt="BACKGROUND">
+    <Bar :favorites="favorites" @open="openApp" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import Apps from "./assets/Apps.json"
+
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Bar,
+    },
+    methods: {
+        openApp(obj) {
+            if(!obj) return false
+            var name = obj.id || obj
+
+            if(!Apps[name]) return false // Future notification
+
+            obj = Apps[name]
+
+            this.ActiveApps[name] = obj
+        },
+        closeApp(obj) {
+            if(!obj) return false
+            var name = obj.id || obj
+
+            if(!this.ActiveApps[name]) return false // Future notification
+
+            delete this.ActiveApps[name]
+        }
+    },
+    data() {
+        // Filter and render of all apps
+        var favorites = {}
+        for (let key in Apps) {
+            var obj = Apps[key]
+            obj.id = key
+            if(obj.icon) { // Icon grabber
+                obj.icon = require(`./assets/images/${obj.icon}`)
+            }
+            if(obj.favorite) { // Always do this last
+                favorites[key] = obj
+            }
+        }
+
+        return {
+            Apps,
+            favorites,
+            ActiveApps: {}
+        }
+    }
 }
 </script>
 
 <style>
+html, body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+
+    width: 100vw;
+    height: 100vh;
+
+    padding: 0;
+    margin: 0;
+}
+
+#backgroundIMG {
+    display:block;
+    width:100%; height:100%;
+    object-fit: cover;
+}
+
+.blurred {
+    background-color: rgba(71, 71, 71, 0.34);
+    backdrop-filter: blur(16px) brightness(115%);
 }
 </style>
---
+++ src/components/System/Bar.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="sys-bottom" class="blurred">
+        <div class="favorites">
+            <div class="app" v-for="(v, i) in favorites" :key="i" @click.stop="open(i)">
+                <img v-if="v.icon" :src="v.icon" alt="Icon" :title="v.title">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Bottom",
+    props: {
+        ActiveApps: Array,
+        favorites: Array
+    },
+    methods: {
+        open(...obj) {
+            this.$emit('open', ...obj)
+        }
+    },
+    mounted() {
+        console.log(this.favorites);
+    }
+}
+</script>
+
+<style scoped>
+#sys-bottom {
+    position: absolute;
+    left: 50%;
+    bottom: 7px;
+    transform: translateX(-50%);
+
+    width: 40vw;
+    height: 74px;
+
+    padding: 0 19px;
+
+    border: 1px solid #707070;
+    border-radius: 17px;
+}
+
+.favorites .app,
+.favorites {
+    position: relative;
+    display: inline-block;
+
+    cursor: pointer;
+}
+
+.favorites .app img {
+    position: relative;
+    width: 70px;
+    height: 70px;
+}
+</style>
